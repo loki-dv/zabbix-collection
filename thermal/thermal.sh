d353968d284c1950648f1669b1bbc8ee1b207dc2
Никита Бабич <nikita.babich@flant.ru>
2016-07-05 20:45:51 +0300

    Correct thermal

--- thermal/thermal.sh
+++ thermal/thermal.sh
@@ -15,7 +15,9 @@ fi
 if [[ $1 == "discovery" ]]; then
   it=0
   printf "{\"data\":[";
-for I in `sensors | grep -in '^Core [0-9]' | awk -F: '{print $2" string "$1"" $3}' | awk '{print $1"_"$2"_"$3"_"$4}'`; do
+#for I in `sensors | grep -in '^Core [0-9]\|^temp[0-9]' | awk -F: '{print $2" string "$1"" $3}' | awk '{print $1"_"$2"_"$3"_"$4}'`; do
+for I in `sensors | grep -i '^Core [0-9]\|^temp[0-9]' | awk -F: '{print "Core_"NR}'`; do
+#sensors | grep -i '^Core [0-9]\|^temp[0-9]' | awk -F: '{print "Core_"NR}'
     if [ $it == 1 ]; then printf ","; fi
       printf "{\"{#ID}\":\"$I\"}"
       it=1
@@ -25,5 +27,4 @@ for I in `sensors | grep -in '^Core [0-9]' | awk -F: '{print $2" string "$1"" $3
 fi
 
 # Get data
-sensors | grep -in '^Core [0-9]' | awk -F: '{print $2" string "$1"" $3}' | awk '{print $1"_"$2"_"$3"_"$4,$5}' | grep "$1" | awk '{print $2}' | sed -e 's/\+//g' | sed -e 's/°C//g'
-
+sensors | grep -i '^Core [0-9]\|^temp[0-9]' | awk -F: '{print "Core_"NR $2}' | grep "$1" | awk '{print $2}' | sed -e 's/\+//g' | sed -e 's/°C//g'
